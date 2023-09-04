99e885b501e572dc1ff7610c963390616e44d542
THEJAS KUMAR.B <84064959+thejas1508@users.noreply.github.com>
2023-09-04 19:38:19 +0530

    Mango_tree.py

---
+++ Mongo_tree.py
@@ -0,0 +1,12 @@
+Fruits taken=int(input("Enter the number of Mangoes Ryan needed to take from tree:"))
+Watchman1share=fruits taken/2
+print("number of fruits watchman1 had got:",watchman1share)
+watchman2share=watchman1share/2
+print("number of fruits watchman2 had got:",watchman2share)
+watchman3share=watchman2share/2
+print("number of fruits watchman3 had got:",watchman3share)
+print("balance fruits Ryan got at last:",watchman3share,"Mango")
+
+       
+ 
+
